f4e7492211330201bd3a3cb5186bf973095790a6
Sion Avakian <avakianssion@gmail.com>
2020-08-18 13:38:04 -0700

    Add files via upload

--- amazon/amazon/items.py
+++ amazon/amazon/items.py
@@ -13,13 +13,7 @@ import re
 #class AmazonItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-'''
-    product_name = scrapy.Field()
-    product_sale_price = scrapy.Field()
-    product_category = scrapy.Field()
-    product_original_price = scrapy.Field()
-    product_availability = scrapy.Field()
-'''
+
 class AmazonItem(scrapy.Item):
 	# define the fields for your item here like:
 	title_Product = scrapy.Field()
--- amazon/amazon/spiders/__init__.py
+++ amazon/amazon/spiders/__init__.py
@@ -6,35 +6,11 @@
 import scrapy
 from amazon.items import AmazonItem
 import re
-
 import sys
-
 from twisted.internet import reactor
-
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 
-'''
-class AmazonProductSpider(scrapy.Spider):
-  name = "AmazonDeals"
-  allowed_domains = ["amazon.com"]
-  
-  #Use working product URL below
-  start_urls = ["https://www.amazon.com/s?k=men%27s+sunglasses" 
-     ]
- 
-  def parse(self, response):
-      items = AmazonItem()
-      title = response.xpath('//h1[@id="title"]/span/text()').extract()
-      sale_price = response.xpath('//span[contains(@id,"ourprice") or contains(@id,"saleprice")]/text()').extract()
-      category = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
-      availability = response.xpath('//div[@id="availability"]//text()').extract()
-      items['product_name'] = ''.join(title).strip()
-      items['product_sale_price'] = ''.join(sale_price).strip()
-      items['product_category'] = ','.join(map(lambda x: x.strip(), category)).strip()
-      items['product_availability'] = ''.join(availability).strip()
-      yield items
-      '''
 class AmazonProductSpider(scrapy.Spider):
 	name = "AmazonDeals"
 	allowed_domains = ["amazon.com"]
